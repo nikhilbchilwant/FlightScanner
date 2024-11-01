823c0ac8057768dfc4a5e4855ae7f9bf0fe76ece
Nikhil Chilwant <nikhilbchilwant@gmail.com>
2024-11-01 09:51:00 +0100

    Restructure the project

--- src/main/java/com/fs/scanner/FlightScannerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.fs.scanner;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/fs/v1")
-public class FlightScannerApplication extends Application {
-
-}
