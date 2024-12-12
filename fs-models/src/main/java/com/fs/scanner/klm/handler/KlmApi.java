01763c0c1808f5a11c0d00dba7e0ddf2ca223747
Nikhil Chilwant <nikhilbchilwant@gmail.com>
2024-12-12 17:06:40 +0100

    Add RestClient

--- fs-models/src/main/java/com/fs/scanner/klm/handler/KlmApi.java
+++ fs-models/src/main/java/com/fs/scanner/klm/handler/KlmApi.java
@@ -2,25 +2,19 @@ package com.fs.scanner.klm.handler;
 
 import com.fs.scanner.klm.model.FlightDetails;
 import com.fs.scanner.klm.model.Offer;
-
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Response;
-
-
-
-
-import java.io.InputStream;
-import java.util.Map;
-import java.util.List;
-
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 
 @Path("/klm/offers")
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", comments = "Generator version: 7.8.0")
+@RegisterRestClient
 public interface KlmApi {
 
     @POST
-    @Consumes({ "application/json" })
-    @Produces({ "application/json" })
+    @Consumes({"application/json"})
+    @Produces({"application/json"})
     Offer offer(FlightDetails flightDetails);
 }
--- fs-service/src/main/java/com/fs/scanner/handler/FlightsApi.java
+++ fs-service/src/main/java/com/fs/scanner/handler/FlightsApi.java
@@ -1,26 +1,24 @@
 package com.fs.scanner.handler;
 
-import com.fs.scanner.model.Flight;
-
-import jakarta.ws.rs.*;
+import com.fs.scanner.klm.handler.KlmApi;
+import com.fs.scanner.klm.model.FlightDetails;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
-
-
-
-
-import java.io.InputStream;
-import java.util.Map;
-import java.util.List;
-
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 
 @Path("/flights")
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", comments = "Generator version: 7.8.0")
 public class FlightsApi {
 
+    @RestClient
+    KlmApi KlmClient;
+
     @GET
-    @Produces({ "application/json" })
-    public Response getFlights(@QueryParam("source")   String source,@QueryParam("destination")   String destination,@QueryParam("travelDate")   String travelDate) {
-        return Response.ok().entity("magic!").build();
+    @Produces({"application/json"})
+    public Response getFlights(@QueryParam("source") String source, @QueryParam("destination") String destination, @QueryParam("travelDate") String travelDate) {
+        return Response.ok().entity(KlmClient.offer(new FlightDetails()).offerPrice(1.0)).build();
     }
 }
