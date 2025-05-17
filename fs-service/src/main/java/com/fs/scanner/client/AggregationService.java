e177f7715a07e9f9e29bc7bd473f81b52a423d0a
Nikhil Chilwant <nikhilbchilwant@gmail.com>
2025-05-17 19:51:20 +0200

    Parallalize call for offer providers

--- fs-service/src/main/java/com/fs/scanner/client/AggregationService.java
+++ fs-service/src/main/java/com/fs/scanner/client/AggregationService.java
@@ -3,42 +3,27 @@ package com.fs.scanner.client;
 import com.fs.scanner.common.handler.OfferProvider;
 import com.fs.scanner.common.model.FlightDetails;
 import com.fs.scanner.common.model.Offer;
-import io.smallrye.context.api.ManagedExecutorConfig;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Any;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.eclipse.microprofile.context.ManagedExecutor;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestScoped
 public class AggregationService {
 
     private static final Log log = LogFactory.getLog(AggregationService.class);
+
     @Inject
     @Any
     Instance<OfferProvider> clients;
-    @Inject
-    @ManagedExecutorConfig(maxAsync = 4)
-    ManagedExecutor aggregationExecutor;
-
-
 
     public List<Offer> getOffers(FlightDetails flightDetails) {
         log.trace("AggregationService bean " + this);
-        Iterator<OfferProvider> iterator = clients.iterator();
-        List<Offer> offers = new ArrayList<>();
-        while (iterator.hasNext()) {
-            OfferProvider provider = iterator.next();
-            Offer offer = provider.call(flightDetails);
-            offers.add(offer);
-        }
-
-        return offers;
+        return clients.stream().parallel().map(offerProvider -> offerProvider.call(flightDetails)).collect(Collectors.toList());
     }
 }
